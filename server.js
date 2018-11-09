da51658f02c21172b972624067b05664afac0e5a
Abhay Agnihotri <abh.k.agnihotri@gmail.com>
2018-11-09 14:47:48 +0530

    changes

--- server.js
+++ server.js
@@ -33,27 +33,7 @@ app.get('/users', function(req, res) {
     }    
       res.status(200).send(data); 
 }); 
-app.get('/users/:num', function(req, res){
-    var users = [];
-    var num = req.params.num;
 
-   if (isFinite(num) && num  > 0 ) {
-     for (i = 0; i <= num-1; i++) {
-       users.push({
-           firstName: faker.name.firstName(),
-           lastName: faker.name.lastName(),
-           username: faker.internet.userName(),
-           email: faker.internet.email()
-        });
-     }
-
-     res.status(200).send(users);
-    
-   } else {
-     res.status(400).send({ message: 'invalid number supplied' });
-   }
-
-});
 // more routes for our API will happen here
 
 
